a60b7dfb3dfbedb1c6859a806063c61f3fb1c2a8
West <agus2kcoin@outlook.com>
2022-12-08 17:51:37 -0300

    Projects setup

---
+++ 01-birthday-reminder/setup/src/List.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const List = ({people}) => {
+  return (
+    <>
+      {people.map(({id, name, age, image}) => {
+        return <article key={id} className="person">
+          <img src={image} alt={name} />
+          <div>
+            <h4>{name}</h4>
+            <p>{age}</p>
+          </div>
+        </article>
+      })}
+    </>
+  );
+};
+
+export default List;
