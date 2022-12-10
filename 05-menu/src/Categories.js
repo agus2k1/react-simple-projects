c29ff7453b0c6611a75fda0dba574cbe6767fca8
West <agus2kcoin@outlook.com>
2022-12-10 19:16:09 -0300

    Project Completed

--- 05-menu/src/Categories.js
+++ 05-menu/src/Categories.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({filterItems, categories}) => {
+
+  return <div className='btn-container'>
+    {categories.map((category, index) => {
+      return <button key={index} className='filter-btn' onClick={() => filterItems(category)}>{category}</button>;
+    })}
+  </div>;
 };
 
 export default Categories;
