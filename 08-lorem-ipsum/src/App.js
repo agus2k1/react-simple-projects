c29f40bf1b2be8807c6e811f759f5f24016946ec
West <agus2kcoin@outlook.com>
2022-12-11 15:24:15 -0300

    Project completed

--- 08-lorem-ipsum/src/App.js
+++ 08-lorem-ipsum/src/App.js
@@ -1,10 +1,38 @@
 import React, { useState } from 'react';
 import data from './data';
+
 function App() {
-  
-  
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if (count <= 0){
+      amount = 1;
+    } else if (count > data.length){
+      amount = data.length;
+    }
+
+    setText(data.slice(0, amount));
+  }
+
   return (
-  <h2>lorem ipsum project setup</h2>
+  <section className='section-center'>
+    <h3>lorem ipsum generator</h3>
+    <form className='lorem-form' onSubmit={handleSubmit}>
+      <label htmlFor='amount'>
+        paragraphs: 
+      </label>
+      <input type='number' name='amount' id='amount' placeholder='0' value={count} onChange={(e) => setCount(e.target.value)}/>
+      <button type='submit' className='btn'>generate</button>
+    </form>
+    <article className='lorem-text'>
+      {text.map((paragraph, index) => {
+        return <p key={index}>{paragraph}</p>
+      })}
+    </article>
+  </section>
     )
 }
 
