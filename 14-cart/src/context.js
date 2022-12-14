e1829284347be15c2df70771744910c03f5d4940
West <agus2kcoin@outlook.com>
2022-12-14 19:59:01 -0300

    Project completed

--- 14-cart/src/context.js
+++ 14-cart/src/context.js
@@ -15,10 +15,50 @@ const initialState = {
 const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  const clearCart = () => {
+    dispatch({ type: "CLEAR_CART" });
+  };
+
+  const remove = (id) => {
+    dispatch({ type: "REMOVE", payload: id });
+  };
+
+  const increase = (id) => {
+    dispatch({ type: "INCREASE", payload: id });
+  };
+
+  const decrease = (id) => {
+    dispatch({ type: "DECREASE", payload: id });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    dispatch({ type: "LOADING" });
+    const response = await fetch(url);
+    const cart = await response.json();
+    dispatch({ type: "DISPLAY_ITEMS", payload: cart });
+  };
+
+  const toggleAmount = (id, type) => {
+    dispatch({ type: "TOGGLE_AMOUNT", payload: { id, type } });
+  };
+
+  useEffect(() => {
+    dispatch({ type: "GET_TOTALS" });
+  }, [state.cart]);
+
   return (
     <AppContext.Provider
       value={{
         ...state,
+        clearCart,
+        remove,
+        increase,
+        decrease,
+        toggleAmount,
       }}
     >
       {children}
--- 14-cart/src/reducer.js
+++ 14-cart/src/reducer.js
@@ -1,5 +1,57 @@
 const reducer = (state, action) => {
-  return state;
+  let tempCart;
+
+  switch (action.type) {
+    case "CLEAR_CART":
+      return { ...state, cart: [] };
+
+    case "REMOVE":
+      return {
+        ...state,
+        cart: state.cart.filter((item) => item.id !== action.payload),
+      };
+
+    case "GET_TOTALS":
+      let { total, amount } = state.cart.reduce(
+        (cartTotal, cartItem) => {
+          const { price, amount } = cartItem;
+          cartTotal.amount += amount;
+          cartTotal.total += price * amount;
+          return cartTotal;
+        },
+        {
+          total: 0,
+          amount: 0,
+        }
+      );
+      total = parseFloat(total.toFixed(2));
+      return { ...state, total, amount };
+
+    case "LOADING":
+      return { ...state, loading: true };
+
+    case "DISPLAY_ITEMS":
+      return { ...state, loading: false, cart: action.payload };
+
+    case "TOGGLE_AMOUNT":
+      let tempCart = state.cart
+        .map((item) => {
+          if (item.id === action.payload.id) {
+            if (action.payload.type === "inc") {
+              return { ...item, amount: item.amount + 1 };
+            } else if (action.payload.type === "dec") {
+              return { ...item, amount: item.amount - 1 };
+            }
+          } else {
+            return item;
+          }
+        })
+        .filter((item) => item.amount !== 0);
+      return { ...state, cart: tempCart };
+
+    default:
+      throw new Error("no matching action type");
+  }
 };
 
 export default reducer;
